896265b8b61ba6c4351412c93619065551b5cdeb
wallaby <36108518+wallabythree@users.noreply.github.com>
2024-12-01 07:06:24 +0100

    Initial solution for day 1

--- src/twenty_four/day01.rs
+++ src/twenty_four/day01.rs
@@ -1,29 +1,76 @@
 use crate::Solution;
+use std::collections::HashMap;
+use std::iter::zip;
 
 pub const SOLUTION: Solution<usize, usize> = Solution { part1, part2 };
 
 fn part1(input: &str) -> usize {
-    input.len()
+    let mut l: Vec<usize> = Vec::new();
+    let mut r: Vec<usize> = Vec::new();
+
+    input.lines().for_each(|line| {
+        let mut iter = line.split_whitespace();
+
+        let a = iter.next().unwrap().parse().unwrap();
+        let b = iter.next().unwrap().parse().unwrap();
+
+        l.push(a);
+        r.push(b);
+    });
+
+    l.sort();
+    r.sort();
+
+    let pairs = zip(l, r);
+
+    pairs.map(|(a, b)| a.abs_diff(b)).sum()
 }
 
 fn part2(input: &str) -> usize {
-    input.len()
+    let mut l: Vec<usize> = Vec::new();
+    let mut r: HashMap<usize, usize> = HashMap::new();
+
+    input.lines().for_each(|line| {
+        let mut iter = line.split_whitespace();
+
+        let a = iter.next().unwrap().parse().unwrap();
+        let b = iter.next().unwrap().parse().unwrap();
+
+        l.push(a);
+
+        if let Some(freq) = r.get_mut(&b) {
+            *freq += 1;
+        } else {
+            r.insert(b, 1);
+        }
+    });
+
+    l.iter()
+        .map(|a| {
+            let f = if let Some(freq) = r.get(a) { *freq } else { 0 };
+            a * f
+        })
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::{part1, part2};
 
-    const TEST_INPUT: &str = "";
+    const TEST_INPUT: &str = "3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3";
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(TEST_INPUT), 0);
+        assert_eq!(part1(TEST_INPUT), 11);
     }
-    
+
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT), TEST_INPUT.len());
+        assert_eq!(part2(TEST_INPUT), 31);
     }
 }
-
