4a73cef20fc609215a7d95ae7840fe7281a2d994
wallaby <36108518+wallabythree@users.noreply.github.com>
2024-12-02 07:40:30 +0100

    Add day 3 scaffolding

--- benches/aoc.rs
+++ benches/aoc.rs
@@ -5,7 +5,7 @@ use solutions;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let year = 2024;
-    let day = 2;
+    let day = 3;
 
     let session_key = env::var("AOC_SESSION").unwrap();
     let client = rudolf_rs::Client::new(String::from(session_key));
